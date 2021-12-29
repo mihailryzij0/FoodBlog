85f0a96663a523952d3f897402a340c31d650108
mihailryzij0 <mihailryzij0@gmail.com>
2021-12-29 14:03:56 +0300

    add slider.scss

--- src/index.js
+++ src/index.js
@@ -3,3 +3,16 @@ import "./feedBack.scss";
 import "./recipePage.scss";
 import "./common-modules/header/header.scss";
 import "./common-modules/footer/footer.scss";
+import {Slider} from "./common-modules/library/slider";
+import "./common-modules/library/slider.scss"
+document.addEventListener('DOMContentLoaded', function () {
+ 
+  new Slider(document.querySelector(".slider"), {
+      autoplay: true,
+      interval: 5000,
+      doots: true
+  });
+});
+
+
+
